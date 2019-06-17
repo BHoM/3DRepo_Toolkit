5c88c38fc9c2fcba9ff43793e40e7051915ec940
carmenfan <wind5320@hotmail.com>
2019-06-17 13:56:35 +0100

    Commiting template

---
+++ 3DRepo_Toolkit/_3DRepo_Toolkit_Engine/Convert/To_3DRepo_Toolkit/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Structure.Elements;
+
+namespace BH.Engine._3DRepo_Toolkit
+{
+    public static partial class Convert
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        //Add methods for converting From BHoM to the specific software types, if possible to do without any BHoM calls
+        //Example:
+        //public static _3DRepo_ToolkitNode To_3DRepo_Toolkit(this Node node)
+        //{
+        //    //Insert code for convertion
+        //}
+
+        /***************************************************/
+    }
+}
