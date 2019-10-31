464566d838072e82beee829449764a0dba19eef7
Alessio Lombardi <alessio.lombardi@burohappold.com>
2019-10-31 16:06:05 +0000

    #10

---
+++ _3DRepo_Toolkit_Adapter/CRUD/Create/_Create.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2019, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Structure.Elements;
+using ThreeDRepo;
+using BH.oM.Base;
+
+namespace BH.Adapter.ThreeDRepo
+{
+    public partial class RepoAdapter
+    {
+        /***************************************************/
+        /**** Adapter overload method                   ****/
+        /***************************************************/
+
+        protected override bool Create<T>(IEnumerable<T> objects)
+        {
+            return true;
+        }
+
+        /***************************************************/
+
+        protected bool Create(IEnumerable<IObject> objects)
+        {
+            Logger.Instance.Log("Create Called");
+            //This is the main dispatcher method, calling the specific implementation methods for the other toolkits.
+
+            bool success = true;        //boolean returning if the creation was successfull or not
+
+            success = CreateCollection(objects as dynamic); //Calls the correct CreateCollection method based on dynamic casting
+            Logger.Instance.Log("Committing changes.");
+            string error = "";
+            success = controller.Commit(ref error);
+            if (success)
+                Logger.Instance.Log("Done.");
+            else
+                BH.Engine.Reflection.Compute.RecordError($"Error when sending data to 3DRepo:\n{error}");
+
+            return success;             //Finally return if the creation was successful or not
+
+        }
+    }
+}
