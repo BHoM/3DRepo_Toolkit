5d3eb15ca6d51a009698ed76692c10b7b44cc63e
Alessio Lombardi <alessio.lombardi@burohappold.com>
2020-08-10 14:09:26 +0100

    Main structure

--- TDRepo_oM/Issue.cs
+++ TDRepo_oM/Issue.cs
@@ -8,24 +8,36 @@ using System.Threading.Tasks;
 
 namespace BH.oM.External.TDRepo
 {
-    public class TDRIssue : BHoMObject
+    public class Issue : IObject // See https://3drepo.github.io/3drepo.io/#api-Issues-newIssue
     {
-        public virtual string Id { get; set; }
+        [Description("The name of the issue.")]
         public virtual string Name { get; set; }
-        public virtual string Priority { get; set; }
-        public virtual string Status { get; set; }
+
+        [Description("The roles assigned to the issue. " +
+            "Even though its an array (for future support of multiple assigned jobs), " +
+            "currently it has one or none elements correspoing to the available jobs in the teamaspace.")]
         public virtual List<string> AssignedRoles { get; set; } = new List<string>();
 
-        [Description("Topic type of the issue.")]
-        public virtual string Type { get; set; }
+        [Description("The status of the issue. It can have a value of `open`,`in progress`,`for approval` or `closed`.")]
+        public virtual string Status { get; set; }
 
-        public virtual object DueDate { get; set; }
+        [Description("The priority of the issue. It can have a value of `none`, `low`, `medium` or `high`.")]
+        public virtual string Priority { get; set; }
+
+        [Description("Topic type of the issue. Its value has to be one of the defined topic_types for the model." +
+            "\nDefaults to `for_information`." +
+            "\nSee https://3drepo.github.io/3drepo.io/#api-Model-createModel for more details.")]
+        public virtual string TopicType { get; set; } = "for_information";
+
+        [Description("The viewpoint of the issue, defining the position of the camera and the screenshot for that position.")]
+        public virtual Viewpoint Viewpoint { get; set; }
+
+        [Description("The description of the created issue.")]
         public virtual string Description { get; set; }
 
-        [Description("X, Y, Z values.")]
+        [Description("The vector defining the pin of the issue (X, Y, Z).")]
         public virtual string[] Position { get; set; }
 
-        [Description("Issue View Point. This includes a path to the file containing an image picturing the issue.")]
-        public virtual string IssueViewPoint { get; set; }
+
     }
 }
