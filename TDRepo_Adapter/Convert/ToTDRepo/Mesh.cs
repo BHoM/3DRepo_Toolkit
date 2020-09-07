b836d999720443e87ae22c74a2a1e62c4474d8a8
Alessio Lombardi <alessio.lombardi@burohappold.com>
2020-09-07 09:39:03 +0100

    Removed unneeded references

--- TDRepo_Adapter/Convert/ToTDRepo/Mesh.cs
+++ TDRepo_Adapter/Convert/ToTDRepo/Mesh.cs
@@ -25,7 +25,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BH.oM.Structure.Elements;
 using BH.oM.Geometry;
 using BH.oM.Adapters.TDRepo;
 
