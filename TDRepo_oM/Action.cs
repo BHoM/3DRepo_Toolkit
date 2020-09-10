c6d9e0cd98a7376f24bb21bdd3582590855adc3a
Alessio Lombardi <alessio.lombardi@burohappold.com>
2020-09-10 15:55:53 +0100

    Added `action` to store Comments with status changes.

---
+++ TDRepo_oM/Action.cs
@@ -0,0 +1,19 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Adapters.TDRepo
+{
+    [Description("Describes an modification done to an existing issue on 3DRepo." +
+        "E.g. property = `status`; from = `inactive`; to = `active`.")]
+    public class Action
+    {
+        public string property { get; set; }
+        public string from { get; set; }
+        public string to { get; set; }
+    }
+}
