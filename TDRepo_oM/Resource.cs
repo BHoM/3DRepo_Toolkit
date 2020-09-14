81a43dc3d52c8f251d1ae61e1a164e200b503995
Alessio Lombardi <alessio.lombardi@burohappold.com>
2020-09-14 17:16:13 +0100

    Adding Resource to Issue

---
+++ TDRepo_oM/Resource.cs
@@ -0,0 +1,18 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Adapters.TDRepo
+{
+    public class Resource
+    {
+        public string _id { get; set; }
+        public string name { get; set; }
+        public int createdAt { get; set; }
+        public int size { get; set; }
+    }
+}
